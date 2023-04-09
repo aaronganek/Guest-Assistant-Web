ce079939669715ed943647cf49e32982eb3d1d58
aaronganek <aaron.ganek@gmail.com>
2023-04-08 19:08:59 -0700

    Use sign-in with redirect instead of popup

--- public/grant_access.js
+++ public/grant_access.js
@@ -1,26 +1,18 @@
 // Import the required Firebase modules
-import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyBNrfpbEe1TP_gbNTd9xGtSh74AFN3a2YQ",
-  authDomain: "guestassistant-378f6.firebaseapp.com",
-  projectId: "guestassistant-378f6",
-  storageBucket: "guestassistant-378f6.appspot.com",
-  messagingSenderId: "186980398604",
-  appId: "1:186980398604:web:726d2be366477e6dc3a029"
+  // Your Firebase configuration here
 };
 
-// Check if any Firebase apps have been initialized
-if (!getApps().length) {
-  // Initialize Firebase if no apps have been initialized
-  initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 // Export the grantAccess function
-export const grantAccess = () => {
+export const grantAccess = async () => {
   // Get DOM elements
   const signInButton = document.getElementById('signInButton');
   const statusMessage = document.getElementById('statusMessage');
@@ -32,37 +24,48 @@ export const grantAccess = () => {
   const auth = getAuth();
   const db = getFirestore();
 
-  // Sign in with Google when the button is clicked
-  signInButton.addEventListener('click', () => {
-    signInWithPopup(auth, provider).then(async (result) => {
-      // Get the token from the URL query parameter
-      const urlParams = new URLSearchParams(window.location.search);
-      const token = urlParams.get('token');
-
-      // Look up the access request information in the "PendingEmails" collection
-      const accessRequestRef = doc(db, "PendingEmails", token);
-      const accessRequestSnapshot = await getDoc(accessRequestRef);
-
-      // Check if the access request exists
-      if (!accessRequestSnapshot.exists()) {
-        statusMessage.textContent = "Access request not found.";
-        return;
-      }
-
-      // Get the access request data
-      const accessRequestData = accessRequestSnapshot.data();
-
-      // Create a new document in the "AuthorizedEmails" collection
-      await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
-
-      // Delete the access request from the "PendingEmails" collection
-      await deleteDoc(accessRequestRef);
-
-      // Display a success message
-      statusMessage.textContent = "Access granted successfully.";
-    }).catch((error) => {
-      // Display an error message
-      statusMessage.textContent = "Error: " + error.message;
-    });
+  // Check for sign-in redirect result
+  getRedirectResult(auth).then(async (result) => {
+    if (result.user) {
+      // User is signed in after the redirect
+      await handleSignIn(result.user, db, statusMessage);
+    }
+  }).catch((error) => {
+    // Display an error message
+    statusMessage.textContent = "Error: " + error.message;
   });
+
+  // Sign in with redirect when the button is clicked
+  signInButton.addEventListener('click', () => {
+    signInWithRedirect(auth, provider);
+  });
+};
+
+// Handle the sign-in process
+const handleSignIn = async (user, db, statusMessage) => {
+  // Get the token from the URL query parameter
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = urlParams.get('token');
+
+  // Look up the access request information in the "PendingEmails" collection
+  const accessRequestRef = doc(db, "PendingEmails", token);
+  const accessRequestSnapshot = await getDoc(accessRequestRef);
+
+  // Check if the access request exists
+  if (!accessRequestSnapshot.exists()) {
+    statusMessage.textContent = "Access request not found.";
+    return;
+  }
+
+  // Get the access request data
+  const accessRequestData = accessRequestSnapshot.data();
+
+  // Create a new document in the "AuthorizedEmails" collection
+  await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
+
+  // Delete the access request from the "PendingEmails" collection
+  await deleteDoc(accessRequestRef);
+
+  // Display a success message
+  statusMessage.textContent = "Access granted successfully.";
 };
