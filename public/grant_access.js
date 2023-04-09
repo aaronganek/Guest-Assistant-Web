889138822b6e5dcaffdfed5955dad37a3dbd1896
aaronganek <aaron.ganek@gmail.com>
2023-04-08 20:02:27 -0700

    Confirm after person clicks sign in

--- public/grant_access.js
+++ public/grant_access.js
@@ -1,6 +1,6 @@
 // Import the required Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
 
 // Your Firebase configuration object
@@ -13,8 +13,8 @@ const firebaseConfig = {
   appId: "1:186980398604:web:726d2be366477e6dc3a029"
 };
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
+// Initialize Firebase if not already initialized
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 // Export the grantAccess function
 export const grantAccess = () => {
@@ -30,41 +30,34 @@ export const grantAccess = () => {
   const db = getFirestore();
 
   // Sign in with Google when the button is clicked
-  signInButton.addEventListener('click', () => {
-    signInWithRedirect(auth, provider);
+  signInWithPopup(auth, provider).then(async (result) => {
+    // Get the token from the URL query parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    // Look up the access request information in the "PendingEmails" collection
+    const accessRequestRef = doc(db, "PendingEmails", token);
+    const accessRequestSnapshot = await getDoc(accessRequestRef);
+
+    // Check if the access request exists
+    if (!accessRequestSnapshot.exists()) {
+      statusMessage.textContent = "Access request not found.";
+      return;
+    }
+
+    // Get the access request data
+    const accessRequestData = accessRequestSnapshot.data();
+
+    // Create a new document in the "AuthorizedEmails" collection
+    await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
+
+    // Delete the access request from the "PendingEmails" collection
+    await deleteDoc(accessRequestRef);
+
+    // Display a success message
+    statusMessage.textContent = "Access granted successfully.";
+  }).catch((error) => {
+    // Display an error message
+    statusMessage.textContent = "Error: " + error.message;
   });
-
-  // Handle the result of the redirect
-  getRedirectResult(auth)
-    .then(async (result) => {
-      // Get the token from the URL query parameter
-      const urlParams = new URLSearchParams(window.location.search);
-      const token = urlParams.get('token');
-
-      // Look up the access request information in the "PendingEmails" collection
-      const accessRequestRef = doc(db, "PendingEmails", token);
-      const accessRequestSnapshot = await getDoc(accessRequestRef);
-
-      // Check if the access request exists
-      if (!accessRequestSnapshot.exists()) {
-        statusMessage.textContent = "Access request not found.";
-        return;
-      }
-
-      // Get the access request data
-      const accessRequestData = accessRequestSnapshot.data();
-
-      // Create a new document in the "AuthorizedEmails" collection
-      await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
-
-      // Delete the access request from the "PendingEmails" collection
-      await deleteDoc(accessRequestRef);
-
-      // Display a success message
-      statusMessage.textContent = "Access granted successfully.";
-    })
-    .catch((error) => {
-      // Display an error message
-      statusMessage.textContent = "Error: " + error.message;
-    });
 };
