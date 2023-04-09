5f1d43486f5f2f236092e88678047f4c1ac133fb
aaronganek <aaron.ganek@gmail.com>
2023-04-08 18:54:22 -0700

    Updating required authorization step: Only admins can grant access

--- public/grant_access.js
+++ public/grant_access.js
@@ -1,6 +1,15 @@
 // Import the required Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc, deleteDoc, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
+
+// Your Firebase configuration object
+const firebaseConfig = {
+  // Your Firebase configuration here
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 // Export the grantAccess function
 export const grantAccess = () => {
@@ -16,8 +25,19 @@ export const grantAccess = () => {
   const db = getFirestore();
 
   // Sign in with Google when the button is clicked
-  signInButton.addEventListener('click', () => {
-    signInWithPopup(auth, provider).then(async (result) => {
+  signInButton.addEventListener('click', async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const userEmail = result.user.email;
+
+      // Check if the user is an admin
+      const adminsQuery = query(collection(db, "Admins"), where("email", "==", userEmail));
+      const adminsSnapshot = await getDocs(adminsQuery);
+      if (adminsSnapshot.empty) {
+        statusMessage.textContent = "You are not authorized to grant access.";
+        return;
+      }
+
       // Get the token from the URL query parameter
       const urlParams = new URLSearchParams(window.location.search);
       const token = urlParams.get('token');
@@ -43,9 +63,9 @@ export const grantAccess = () => {
 
       // Display a success message
       statusMessage.textContent = "Access granted successfully.";
-    }).catch((error) => {
+    } catch (error) {
       // Display an error message
       statusMessage.textContent = "Error: " + error.message;
-    });
+    }
   });
 };
