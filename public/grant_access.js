b654d232f33a1111954f3264c07ac032ac7f323f
aaronganek <aaron.ganek@gmail.com>
2023-04-08 19:41:13 -0700

    Pretty page again, with updated JS to make it work

--- public/grant_access.js
+++ public/grant_access.js
@@ -5,14 +5,19 @@ import { getFirestore, doc, getDoc, setDoc, deleteDoc } from "https://www.gstati
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  // Your Firebase configuration here
+  apiKey: "AIzaSyBNrfpbEe1TP_gbNTd9xGtSh74AFN3a2YQ",
+  authDomain: "guestassistant-378f6.firebaseapp.com",
+  projectId: "guestassistant-378f6",
+  storageBucket: "guestassistant-378f6.appspot.com",
+  messagingSenderId: "186980398604",
+  appId: "1:186980398604:web:726d2be366477e6dc3a029"
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
 // Export the grantAccess function
-export const grantAccess = async () => {
+export const grantAccess = () => {
   // Get DOM elements
   const signInButton = document.getElementById('signInButton');
   const statusMessage = document.getElementById('statusMessage');
@@ -24,48 +29,42 @@ export const grantAccess = async () => {
   const auth = getAuth();
   const db = getFirestore();
 
-  // Check for sign-in redirect result
-  getRedirectResult(auth).then(async (result) => {
-    if (result.user) {
-      // User is signed in after the redirect
-      await handleSignIn(result.user, db, statusMessage);
-    }
-  }).catch((error) => {
-    // Display an error message
-    statusMessage.textContent = "Error: " + error.message;
-  });
-
-  // Sign in with redirect when the button is clicked
+  // Sign in with Google when the button is clicked
   signInButton.addEventListener('click', () => {
     signInWithRedirect(auth, provider);
   });
-};
 
-// Handle the sign-in process
-const handleSignIn = async (user, db, statusMessage) => {
-  // Get the token from the URL query parameter
-  const urlParams = new URLSearchParams(window.location.search);
-  const token = urlParams.get('token');
+  // Handle the result of the redirect
+  getRedirectResult(auth)
+    .then(async (result) => {
+      // Get the token from the URL query parameter
+      const urlParams = new URLSearchParams(window.location.search);
+      const token = urlParams.get('token');
 
-  // Look up the access request information in the "PendingEmails" collection
-  const accessRequestRef = doc(db, "PendingEmails", token);
-  const accessRequestSnapshot = await getDoc(accessRequestRef);
+      // Look up the access request information in the "PendingEmails" collection
+      const accessRequestRef = doc(db, "PendingEmails", token);
+      const accessRequestSnapshot = await getDoc(accessRequestRef);
 
-  // Check if the access request exists
-  if (!accessRequestSnapshot.exists()) {
-    statusMessage.textContent = "Access request not found.";
-    return;
-  }
+      // Check if the access request exists
+      if (!accessRequestSnapshot.exists()) {
+        statusMessage.textContent = "Access request not found.";
+        return;
+      }
 
-  // Get the access request data
-  const accessRequestData = accessRequestSnapshot.data();
+      // Get the access request data
+      const accessRequestData = accessRequestSnapshot.data();
 
-  // Create a new document in the "AuthorizedEmails" collection
-  await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
+      // Create a new document in the "AuthorizedEmails" collection
+      await setDoc(doc(db, "AuthorizedEmails", accessRequestData.email), accessRequestData);
 
-  // Delete the access request from the "PendingEmails" collection
-  await deleteDoc(accessRequestRef);
+      // Delete the access request from the "PendingEmails" collection
+      await deleteDoc(accessRequestRef);
 
-  // Display a success message
-  statusMessage.textContent = "Access granted successfully.";
+      // Display a success message
+      statusMessage.textContent = "Access granted successfully.";
+    })
+    .catch((error) => {
+      // Display an error message
+      statusMessage.textContent = "Error: " + error.message;
+    });
 };
